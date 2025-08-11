18768eaf14643d2a010365d1a14fdb901dd0e425
Durjoy96 <mddurjoy853@gmail.com>
2025-08-11 15:06:20 +0600

    initial setup complete

---
+++ src/app/home-page-sections/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div>
+      <h1 className="font-pacifico text-6xl font-normal text-base-content">
+        Happy Birthday, Rapunzel!{" "}
+      </h1>
+      <p className="font-quicksand mt-12 text-lg font-normal">
+        You make the world brighter just by being in it. This is your special
+        little place.
+      </p>
+    </div>
+  );
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Hero from "./home-page-sections/Hero";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+}
